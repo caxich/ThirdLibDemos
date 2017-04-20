24d5221f82ab3627a6ef17a4a67b21ab9760d10c
caxich <746135024@qq.com>
2017-04-20 15:04:53 +0800

    ToolBar的使用

--- ToolBarActivity/app/src/main/java/com/cxc/toolbaractivity/MainActivity.java
+++ ToolBarActivity/app/src/main/java/com/cxc/toolbaractivity/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,9 +25,35 @@ public class MainActivity extends AppCompatActivity {
         tb = (Toolbar) findViewById(R.id.tb);
 
         //自定义toolbar属性的代码必须在setSupportActionBar调用之前执行，否则属性设定无效
-        tb.setTitle("ToolBar Demo");
-        tb.setTitleTextColor(ContextCompat.getColor(MainActivity.this,R.color.white));
+//        tb.setTitle("ToolBar Demo");
+//        tb.setTitleTextColor(ContextCompat.getColor(MainActivity.this,R.color.white));
 
         setSupportActionBar(tb);
+    }
+
+    //toolbar自定义菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    //toolbar自定义菜单点击响应事件
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.tb_add:
+                Toast.makeText(this,"add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tb_search:
+                Toast.makeText(this,"search",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tb_menu:
+                Toast.makeText(this,"menu",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
